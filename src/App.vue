0a79b776a12aaac610ea654ed9661d29d9019f98
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-11-11 16:27:31 +0100

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
-import '@/assets/typography.css'
-import '@/assets/spacing.css'
-import AppHeader from './components/layout/AppHeader.vue'
-import { RouterView } from 'vue-router'
+import '@/assets/typography.css';
+import '@/assets/spacing.css';
+
+import { shallowRef } from 'vue';
+import PageLayout from '@/layouts/Page.vue';
+import PlainLayout from '@/layouts/Plain.vue';
+import { useRoute, useRouter, RouterView } from 'vue-router';
+
+const layouts = {
+  plain: PlainLayout,
+  page: PageLayout,
+};
+
+const router = useRouter();
+const currentLayout = shallowRef(PlainLayout);
+
+const setLayout = () => {
+  type LayoutKey = keyof typeof layouts;
+  const newLayout = useRoute()?.meta?.layout as LayoutKey;
+  currentLayout.value = layouts[newLayout] || PlainLayout;
+};
+
+router.afterEach(setLayout);
 </script>
 
 <template>
   <div>
-    <header class="col-span-full">
-      <AppHeader />
-    </header>
-    <main>
+    <component :key="currentLayout" v-bind:is="currentLayout">
       <RouterView />
-    </main>
+    </component>
   </div>
 </template>
---
+++ src/layouts/Page.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AppHeader from '@/components/layout/AppHeader.vue';
+</script>
+
+<template>
+  <div>
+    <header class="col-span-full">
+      <AppHeader />
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
