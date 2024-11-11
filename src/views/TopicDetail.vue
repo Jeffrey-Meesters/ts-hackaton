5ba87e0d03c9d24a21fcaa6791c73daee9cd24f3
Erkan Atesli <erkan.atesli@nl.abnamro.com>
2024-11-11 16:27:58 +0100

    update

--- src/views/TopicDetail.vue
+++ src/views/TopicDetail.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import CodeBlock from '@/components/detail/CodeBlock.vue'
+
+import Card from 'primevue/card'
+
 // import { useDataStore } from '@/stores/useDataStore'
 // const dataStore = useDataStore()
 // const data = dataStore.activeData
@@ -36,21 +39,24 @@ const partialUser: Partial<User> = { name: "Alice" }; // id is optional
 </script>
 
 <template>
-  <div class="h-[10vh] flex justify-center items-center bg-[#007acc]">
+  <div class="mb-4 h-[10vh] flex justify-center items-center bg-[#007acc]">
     <h1 class="text-6xl text-white">{{ data.name }}</h1>
   </div>
-  <article class="w-3/4 mx-auto pt-4">
-    <p>
+  <article class="w-3/4 mx-auto">
+    <p class="mb-4">
       {{ data.description }}
     </p>
 
-    <div class="pt-4" v-for="subTopic in data.subTopics" :key="subTopic.name">
-      <h2 class="mb-0">{{ subTopic.name }}</h2>
-      <p>{{ subTopic.description }}</p>
-
-      <div v-for="example in subTopic.examples" :key="example.title">
-        <CodeBlock :title="example.title" :code="example.code" />
-      </div>
-    </div>
+    <Card v-for="subTopic in data.subTopics" :key="subTopic.name">
+      <template #title>
+        <h2 class="mb-0">{{ subTopic.name }}</h2>
+      </template>
+      <template #content>
+        <p>{{ subTopic.description }}</p>
+        <div v-for="example in subTopic.examples" :key="example.title">
+          <CodeBlock :title="example.title" :code="example.code" />
+        </div>
+      </template>
+    </Card>
   </article>
 </template>
