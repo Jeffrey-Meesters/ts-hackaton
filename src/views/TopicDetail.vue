2efeb72f1bf44bef74dcc9188c30e2aa8923e47e
Erkan Atesli <erkan.atesli@nl.abnamro.com>
2024-11-11 15:56:37 +0100

    feat: mocked data topic detail

--- src/views/TopicDetail.vue
+++ src/views/TopicDetail.vue
@@ -1,8 +1,40 @@
 <script setup lang="ts">
 import CodeBlock from '@/components/detail/CodeBlock.vue'
-import { useDataStore } from '@/stores/useDataStore'
-const dataStore = useDataStore()
-const data = dataStore.activeData
+// import { useDataStore } from '@/stores/useDataStore'
+
+// const dataStore = useDataStore()
+// const data = dataStore.activeData
+const data = {
+  name: 'Utility Types',
+  description: 'Predefined types to facilitate common type transformations.',
+  level: 'advanced',
+  code: '',
+  subTopics: [
+    {
+      name: 'Partial<T>',
+      tags: ['utility', 'generic'],
+      description:
+        'Constructs a type with all properties of T set to optional.',
+      level: 'basic',
+      examples: [
+        {
+          title: 'Basic Usage',
+          description:
+            'Use `Partial<T>` to make all properties of an interface optional.',
+          code: `
+interface User {
+  id: number;
+  name: string;
+}
+
+const partialUser: Partial<User> = { name: "Alice" }; // id is optional
+              `,
+        },
+      ],
+    },
+  ],
+}
+console.log(data)
 </script>
 
 <template>
@@ -10,16 +42,18 @@ const data = dataStore.activeData
     <h1 class="text-6xl text-white">topic</h1>
   </div>
   <article>
-    <h1>Generics</h1>
+    <h1>{{ data.name }}</h1>
     <p>
-      Lorem ipsum for some info about this topic Lorem ipsum for some info about
-      this topic Lorem ipsum for some info about this topic Lorem ipsum for some
-      info about this topic Lorem ipsum for some info about this topic Lorem
-      ipsum for some info about this topic
+      {{ data.description }}
     </p>
 
-    <CodeBlock title="Interface" code="const name = (name: string) => ({})" />
-    {{ data }}
-    <p></p>
+    <div v-for="subTopic in data.subTopics" :key="subTopic.name">
+      <h2>{{ subTopic.name }}</h2>
+      <p>{{ subTopic.description }}</p>
+
+      <div v-for="example in subTopic.examples" :key="example.title">
+        <CodeBlock :title="example.title" :code="example.code" />
+      </div>
+    </div>
   </article>
 </template>
