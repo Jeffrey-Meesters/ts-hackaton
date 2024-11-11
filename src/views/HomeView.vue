7bc13e70f81a32a98b78d9d7d1e6065731b0a544
Ali Gok <ali.goek@nl.abnamro.com>
2024-11-11 16:00:23 +0100

    added tree on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TopicTree from '@/components/topicTree/TopicTree.vue'
+
+</script>
 
 <template>
   <section class="md:max-w-4xl">
     <h1>Homepage</h1>
+    <TopicTree></TopicTree>
   </section>
 </template>
