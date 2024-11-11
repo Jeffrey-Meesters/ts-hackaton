c40d68775e8d03300e70190f984b48d131a51571
Jelle Rienstra <jelle.rienstra@nl.abnamro.com>
2024-11-11 15:15:48 +0100

    detail page

--- src/views/TopicDetail.vue
+++ src/views/TopicDetail.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{ topic: string }>()
+const topic = props.topic || 'Default Topic'
+</script>
 
 <template>
-  <div>
-    <h1>Topic detail view</h1>
+  <div class="h-[30vh] flex justify-center items-center bg-[#007acc]">
+    <h1 class="text-6xl text-white">{{ topic }}</h1>
   </div>
+  <main class="p-5">
+    <!-- Additional content goes here -->
+  </main>
 </template>
