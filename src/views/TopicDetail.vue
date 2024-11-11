862ecede710ed644d7e1d6bef2f344284194ea01
Erkan Atesli <erkan.atesli@nl.abnamro.com>
2024-11-11 15:33:08 +0100

    feat: topic detail

--- src/views/TopicDetail.vue
+++ src/views/TopicDetail.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
 import CodeBlock from '@/components/detail/CodeBlock.vue'
-import dataStore from '@/store/dataStore'
-const route = useRoute()
-
+import { useDataStore } from '@/stores/useDataStore'
 const props = defineProps<{ topic: string }>()
-const topic = props.topic || 'Default Topic'
-const subTopic = route.params.subTopic
-
-const data = dataStore.data[topic].subTopics[subTopic] || null
+const dataStore = useDataStore()
+const data = dataStore.data
 </script>
 
 <template>
