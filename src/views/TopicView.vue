23c472527025f3451bf21af1e292461119c32949
Erkan Atesli <erkan.atesli@nl.abnamro.com>
2024-11-12 14:52:47 +0100

    update topic view

--- src/views/TopicView.vue
+++ src/views/TopicView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import pageLayout from '@/components/pageLayout.vue'
 import { Card } from 'primevue'
+import Accordion from 'primevue/accordion'
+import AccordionPanel from 'primevue/accordionpanel'
+import AccordionHeader from 'primevue/accordionheader'
+import AccordionContent from 'primevue/accordioncontent'
 import { useDataStore } from '@/stores/useDataStore'
 import { storeToRefs } from 'pinia'
 import type { Topic } from '@/types/DataModel'
@@ -14,19 +18,24 @@ const data = computed(() => activeData.value as Topic)
   <pageLayout :name="data.name">
     <template #content>
       <article class="w-3/4 mx-auto">
-        <div class="mb-2">
-          <Button class="mr-2" label="First example" severity="secondary" />
-          <Button label="Second example" severity="secondary" />
-        </div>
         <Card>
           <template #title>
             <h2 class="mb-0">{{ data.name }}</h2>
           </template>
           <template #content>
             <p>{{ data.description }}</p>
-            <div v-for="sub in data.subTopics" :key="sub.name">
-              {{ sub.name }}
-            </div>
+            <Accordion value="0">
+              <AccordionPanel
+                v-for="tab in data.subTopics"
+                :key="tab.name"
+                :value="tab.name"
+              >
+                <AccordionHeader>{{ tab.name }}</AccordionHeader>
+                <AccordionContent>
+                  <p class="m-0">{{ tab.description }}</p>
+                </AccordionContent>
+              </AccordionPanel>
+            </Accordion>
           </template>
         </Card>
       </article>
