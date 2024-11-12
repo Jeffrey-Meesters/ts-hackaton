9ad6d52aad7d1b78bbba2893fad929279267a1e1
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-11-12 15:23:00 +0100

    Update spacing

--- src/components/pageLayout.vue
+++ src/components/pageLayout.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 type props = {
-  name: string
-}
-defineProps<props>()
+  name: string;
+};
+defineProps<props>();
 </script>
 
 <template>
-  <div class="w-full h-screen bg-slate-300">
-    <div class="mb-4 h-[10vh] flex justify-center items-center bg-[#007acc]">
+  <div class="w-full h-screen bg-slate-300 -mt-12 mb-12">
+    <div class="h-[10vh] flex justify-center items-center bg-[#007acc]">
       <h1 class="text-6xl text-white">{{ name }}</h1>
     </div>
     <slot name="content"></slot>
