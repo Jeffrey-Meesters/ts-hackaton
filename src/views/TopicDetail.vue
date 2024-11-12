33e57f3c2801d1ad47cd910d022337d663f23b09
Erkan Atesli <erkan.atesli@nl.abnamro.com>
2024-11-12 14:45:17 +0100

    update accordion

--- src/views/TopicDetail.vue
+++ src/views/TopicDetail.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import CodeBlock from '@/components/detail/CodeBlock.vue';
-import pageLayout from '@/components/pageLayout.vue';
-import Card from 'primevue/card';
-import Button from 'primevue/button';
-import Accordion from 'primevue/accordion';
-import AccordionPanel from 'primevue/accordionpanel';
-import AccordionHeader from 'primevue/accordionheader';
-import AccordionContent from 'primevue/accordioncontent';
+import CodeBlock from '@/components/detail/CodeBlock.vue'
+import pageLayout from '@/components/pageLayout.vue'
+import Card from 'primevue/card'
+import Button from 'primevue/button'
+import Accordion from 'primevue/accordion'
+import AccordionPanel from 'primevue/accordionpanel'
+import AccordionHeader from 'primevue/accordionheader'
+import AccordionContent from 'primevue/accordioncontent'
 
 // import { useDataStore } from '@/stores/useDataStore'
 // const dataStore = useDataStore()
@@ -20,19 +20,20 @@ const data = {
     {
       name: 'Partial<T>',
       tags: ['utility', 'generic'],
-      description: 'Constructs a type with all properties of T set to optional.',
+      description:
+        'Constructs a type with all properties of T set to optional.',
       level: 'basic',
       examples: [
         {
           title: 'Basic Usage',
-          description: 'Use `Partial<T>` to make all properties of an interface optional.',
-          code:
-            '\ninterface User {\n  id: number;\n  name: string;\n}\n\nconst partialUser: Partial<User> = { name: "Alice" }; // id is optional\n              ',
+          description:
+            'Use `Partial<T>` to make all properties of an interface optional.',
+          code: '\ninterface User {\n  id: number;\n  name: string;\n}\n\nconst partialUser: Partial<User> = { name: "Alice" }; // id is optional\n              ',
         },
       ],
     },
   ],
-};
+}
 </script>
 
 <template>
@@ -49,13 +50,21 @@ const data = {
           </template>
           <template #content>
             <p>{{ data.subTopics[0].description }}</p>
-            <div v-for="example in data.subTopics[0].examples" :key="example.title">
+            <div
+              v-for="example in data.subTopics[0].examples"
+              :key="example.title"
+            >
               <CodeBlock :title="example.title" :code="example.code" />
             </div>
           </template>
         </Card>
-        <Accordion value="0" class="mt-8">
-          <AccordionPanel v-for="tab in data.subTopics" :key="tab.name" :value="tab.name">
+        <div class="mt-8"></div>
+        <Accordion value="0">
+          <AccordionPanel
+            v-for="tab in data.subTopics"
+            :key="tab.name"
+            :value="tab.name"
+          >
             <AccordionHeader>{{ tab.name }}</AccordionHeader>
             <AccordionContent>
               <p class="m-0">{{ tab.description }}</p>
