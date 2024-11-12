8a4fd341501b298ac11ff65f28bb6b4fb2b97c9a
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-11-12 15:25:42 +0100

    Update pagelayout

--- src/components/pageLayout.vue
+++ src/components/pageLayout.vue
@@ -6,8 +6,8 @@ defineProps<props>();
 </script>
 
 <template>
-  <div class="w-full h-screen bg-slate-300 -mt-12 mb-12">
-    <div class="h-[10vh] flex justify-center items-center bg-[#007acc]">
+  <div class="w-full h-screen bg-slate-300 -mt-12">
+    <div class="h-[10vh] mb-12 flex justify-center items-center bg-[#007acc]">
       <h1 class="text-6xl text-white">{{ name }}</h1>
     </div>
     <slot name="content"></slot>
