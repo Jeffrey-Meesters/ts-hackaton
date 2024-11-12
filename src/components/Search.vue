3f2e9a474e76ec881d69a7a12ec25b447d09d138
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-11-12 16:14:08 +0100

    some styles

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,90 +1,89 @@
 <template>
-    <div 
-      class='inline-block bg-slate-200 w-full'
-      ref="searchContainer"
-    >
-      <form class="mx-auto">   
-        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-        <div class="relative">
-          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-          </div>
-          <input 
-            v-model="query" 
-            @input="onInput" 
-            @focus="showResults = true" 
-            type="search" 
-            id="default-search" 
-            class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Search..." 
-            required 
-          />
-          <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+  <div class="inline-block w-full" ref="searchContainer">
+    <form class="mx-auto">
+      <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+      <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+          </svg>
         </div>
-      </form>
-      <div v-if="showResults">
-        <span v-if="topics" class="bg-slate-700 text-white block py-2 pl-2">Topics</span>
-        <ul v-if="topics">
-          <li :key="key" v-for="(topic, key) of topics" class='py-1 pl-2' @click="showResults=false">
-            <router-link :to=key>{{ topic?.name }}</router-link>
-          </li>
-        </ul>
+        <input
+          v-model="query"
+          @input="onInput"
+          @focus="showResults = true"
+          type="search"
+          id="default-search"
+          class="block w-full p-6 px-12 text-xl ps-10 text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 d"
+          placeholder="Search..."
+          required
+        />
+        <button
+          type="submit"
+          class="text-white absolute end-5 bottom-5 bg-red-400 rounded-full hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2"
+        >
+          Search
+        </button>
       </div>
+    </form>
+    <div v-if="showResults" class="bg-slate-200 mt-3 shadow-xl rounded-xl overflow-hidden">
+      <span v-if="topics" class="bg-slate-700 text-white block py-2 pl-2">Topics</span>
+      <ul v-if="topics">
+        <li :key="key" v-for="(topic, key) of topics" class="py-1 pl-2" @click="showResults = false">
+          <router-link :to="key">{{ topic?.name }}</router-link>
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, onMounted, onBeforeUnmount } from 'vue';
-  import { docs } from '../data/docs';
-  import type { PartialTypeScriptDocs, SubTopic } from '@/types/DataModel'
+  </div>
+</template>
 
-  type listType = string[];
-  const query = ref<string>('');
-  const searchList = ref<listType>([]);
-  const showResults = ref<boolean>(false);
-  const searchContainer = ref<HTMLElement | null>(null);
-  const topics = ref<PartialTypeScriptDocs | null>(null);
-  const subTopics = ref<SubTopic[] | null>(null);
-  
-  const fetchSearchResults = () => {
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { docs } from '../data/docs';
+import type { PartialTypeScriptDocs, SubTopic } from '@/types/DataModel';
 
-    const result = Object.entries(docs).filter(([key, value]) => {
-        if(value.subTopics.length){
-            const subtopicList = value.subTopics.filter(item => {
-                if(item.name.toLowerCase().includes(query.value.toLowerCase())){
-                    return item as SubTopic;
-                }
-            })
-            subTopics.value = subtopicList;
+type listType = string[];
+const query = ref<string>('');
+const searchList = ref<listType>([]);
+const showResults = ref<boolean>(false);
+const searchContainer = ref<HTMLElement | null>(null);
+const topics = ref<PartialTypeScriptDocs | null>(null);
+const subTopics = ref<SubTopic[] | null>(null);
+
+const fetchSearchResults = () => {
+  const result = Object.entries(docs).filter(([key, value]) => {
+    if (value.subTopics.length) {
+      const subtopicList = value.subTopics.filter((item) => {
+        if (item.name.toLowerCase().includes(query.value.toLowerCase())) {
+          return item as SubTopic;
         }
-        if(value.name.toLowerCase().includes(query.value.toLowerCase())){
-            return [key, value]
-        }
-    })
-    topics.value = Object.fromEntries(result) as PartialTypeScriptDocs;
-    console.log(subTopics.value);
-  };
+      });
+      subTopics.value = subtopicList;
+    }
+    if (value.name.toLowerCase().includes(query.value.toLowerCase())) {
+      return [key, value];
+    }
+  });
+  topics.value = Object.fromEntries(result) as PartialTypeScriptDocs;
+  console.log(subTopics.value);
+};
 
 const onInput = () => {
-    showResults.value = query.value.length > 0;
-    fetchSearchResults();
-}
+  showResults.value = query.value.length > 0;
+  fetchSearchResults();
+};
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (searchContainer.value && !searchContainer.value.contains(event.target as Node)) {
-      showResults.value = false;
-    }
-  };
-  
-  onMounted(() => {
-    document.addEventListener('click', handleClickOutside);
-  });
-  
-  onBeforeUnmount(() => {
-    document.removeEventListener('click', handleClickOutside);
-  });
-  
-  </script>
-  
+const handleClickOutside = (event: MouseEvent) => {
+  if (searchContainer.value && !searchContainer.value.contains(event.target as Node)) {
+    showResults.value = false;
+  }
+};
+
+onMounted(() => {
+  document.addEventListener('click', handleClickOutside);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', handleClickOutside);
+});
+</script>
