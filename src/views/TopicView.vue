91523baf1efde55b21a1bbe525c0a6b93dbe1bcd
Jeffrey Meesters <jeffrey.meesters@nl.abnamro.com>
2024-11-12 14:45:19 +0100

    Show data in topicView

--- src/views/TopicView.vue
+++ src/views/TopicView.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
 import pageLayout from '@/components/pageLayout.vue'
+import { Card } from 'primevue'
 import { useDataStore } from '@/stores/useDataStore'
 import { storeToRefs } from 'pinia'
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-const topic = route.params.topic
+import type { Topic } from '@/types/DataModel'
+import { computed } from 'vue'
 
 const { activeData } = storeToRefs(useDataStore())
+const data = computed(() => activeData.value as Topic)
 </script>
 
 <template>
   <pageLayout :name="activeData.name">
     <template #content>
-      {{ activeData }}
+      <article class="w-3/4 mx-auto">
+        <div class="mb-2">
+          <Button class="mr-2" label="First example" severity="secondary" />
+          <Button label="Second example" severity="secondary" />
+        </div>
+        <Card>
+          <template #title>
+            <h2 class="mb-0">{{ data.name }}</h2>
+          </template>
+          <template #content>
+            <p>{{ data.description }}</p>
+            <div v-for="sub in data.subTopics" :key="sub.name">
+              {{ sub.name }}
+            </div>
+          </template>
+        </Card>
+      </article>
     </template>
   </pageLayout>
 </template>
